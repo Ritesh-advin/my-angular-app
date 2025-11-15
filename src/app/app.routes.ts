5b98d7ba4996fd6a389e3ff79e47b523ef2e20e4
Sumeet Goliwar <sgoliwar@advinsoftwares.com>
2025-11-15 13:03:59 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Contact } from './pages/contact/contact';
+import { Services } from './pages/services/services';
+
+export const routes: Routes = [
+    {path: '', component: Home},
+    {path:'home', component: Home},
+    {path:'about', component: About},
+    {path:'contact', component: Contact},
+    {path:'services', component: Services}
+
+];
